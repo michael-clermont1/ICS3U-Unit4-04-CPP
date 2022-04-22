c08ccbdcd0d4c3645151af7543be4543308d429d
Michael Clermont <michael.clermont@mths.ca>
2022-04-22 18:37:02 +0000

    added guessing game program

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -6,26 +6,40 @@
 
 #include <iostream>
 #include <random>
+#include <string>
 
 int main() {
     // this function is a guessing game
+    std::string numberAsString;
     int numberFromUser;
     int randomNumber;
+    int loopCounter = 0;
 
     // input
-    std::cout << "Enter a number between 0-9: ";
-    std::cin >> numberFromUser;
     std::random_device rseed;
     std::mt19937 rgen(rseed());
     std::uniform_int_distribution<int> idist(0, 9);
     randomNumber = idist(rgen);
-    std::cout << "" << std::endl;
 
     // process & output
-    if (numberFromUser == randomNumber) {
-        std::cout << "You guessed it correctly!";
-    } else {
-        std::cout << "You guessed it incorrectly!";
+    while (true) {
+        std::cout << "Enter a number between 0-9: ";
+        std::cin >> numberAsString;
+        try {
+            numberFromUser = std::stoi(numberAsString);
+            loopCounter = loopCounter + 1;
+            if (numberFromUser == randomNumber) {
+                std::cout << "\nYou guessed correctly in " << loopCounter <<
+                " times!" << std::endl;
+                break;
+            } else if (randomNumber > numberFromUser) {
+                std::cout << "\nNext time guess higher!" << std::endl;
+            } else if (randomNumber < numberFromUser) {
+                std::cout << "\nNext time guess lower!" << std::endl;
+            }
+        } catch (std::invalid_argument) {
+            std::cout << "That is not an integer, guess again" << std::endl;
+        }
     }
     std::cout << "\nDone." << std::endl;
 }
